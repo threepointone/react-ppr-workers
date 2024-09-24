d55600b22c98b53455e0384d9fde0908a07c92a0
Sunil Pai <spai@cloudflare.com>
2024-09-24 12:15:00 +0100

    script tag in head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,25 +23,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* todo: add inter font? */}
-      <title>{metadata.title}</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content={metadata.description} />
-      <meta name="og:title" content={metadata.openGraph.title} />
-      <meta name="og:description" content={metadata.openGraph.description} />
-      <meta name="og:url" content={metadata.openGraph.url} />
-      <meta name="og:siteName" content={metadata.openGraph.siteName} />
-      <meta name="og:image" content={metadata.openGraph.image} />
-      <meta name="twitter:card" content={metadata.twitter.card} />
-      <meta name="twitter:site" content={metadata.twitter.site} />
-      <meta name="twitter:creator" content={metadata.twitter.creator} />
-      <meta name="twitter:image" content={metadata.twitter.image} />
-      <meta charSet="utf-8" />
-      <link rel="stylesheet" href="/dist/styles.css" />
+      <head>
+        {/* todo: add inter font? */}
+        <title>{metadata.title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content={metadata.description} />
+        <meta name="og:title" content={metadata.openGraph.title} />
+        <meta name="og:description" content={metadata.openGraph.description} />
+        <meta name="og:url" content={metadata.openGraph.url} />
+        <meta name="og:siteName" content={metadata.openGraph.siteName} />
+        <meta name="og:image" content={metadata.openGraph.image} />
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:site" content={metadata.twitter.site} />
+        <meta name="twitter:creator" content={metadata.twitter.creator} />
+        <meta name="twitter:image" content={metadata.twitter.image} />
+        <meta charSet="utf-8" />
+        <link rel="stylesheet" href="/dist/styles.css" />
+        <script src="/dist/index.js" type="module" />
+      </head>
       <body>
         <div id="root">{children}</div>
       </body>
-      <script src="/dist/index.js" type="module" />
     </html>
   );
 }
